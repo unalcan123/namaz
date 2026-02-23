c0dbdf847b541090126da770b5e2315143b6a61c
unalcan123 <34675955+unalcan123@users.noreply.github.com>
2026-02-23 03:15:13 +0100

    Rebuild docs clean (remove nested assets)

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -890,7 +890,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d9cf2013464630724a2591726e4cd82a
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "a24a64a4cdd027a5097ab1fe7b1acab8",
+"flutter_bootstrap.js": "e9f59ddc05164266d80c554cc979987f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
